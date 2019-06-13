2d5f7a4db0b75e6b5647667a99f22470b14b06cf
Natalie Perpepaj <perpepajn25@gmail.com>
2019-06-13 16:33:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const questions = [
   {questionText: "Lightning never strikes in the same place twice", answer: false},
   {questionText: "Humans can distinguish between over one trillion different smells", answer: true},
-  {questionText: "Goldfish only have a memory of about three seconds", answer: false}
+  {questionText: "Goldfish are cool", answer: false}
 ]
 
 let question;
